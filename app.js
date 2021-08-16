464c7b62d52ad15be47634068aaa27c5c6b1a7c8
Hwang yena <center7332@naver.com>
2021-08-16 17:51:43 +0900

    day 06 - calculate

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-function btnLog(obj){
-    console.log(typeof obj);
-}
+const x = 1
+const y = 2
+
+console.log(`x + y = ${x+y}`);
+console.log(`x - y = ${x-y}`);
+console.log(`x * y = ${x*y}`);
+console.log(`x / y = ${x/y}`);

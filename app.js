b4a428cd4af9a27cc6048ada68e752bb5ceb4a51
Hwang yena <center7332@naver.com>
2021-08-18 20:04:55 +0900

    day07 - timer

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
-const x = 1
-const y = 2
+let hourSection = document.getElementById("hour");
+let minSection = document.getElementById("min");
+let secSection = document.getElementById("sec");
 
-console.log(`x + y = ${x+y}`);
-console.log(`x - y = ${x-y}`);
-console.log(`x * y = ${x*y}`);
-console.log(`x / y = ${x/y}`);
+let hour = 0;
+let min = 0;
+let sec = 0;
+
+let startTimer;
+
+//타이머 증가
+function setTime(){
+    sec++;
+    if(sec>=60){
+        sec = 0;
+        min += 1;
+    }
+    if(min>=60){
+        min = 0;
+        hour += 1;
+    }
+    printTime();
+}
+
+//타이머 보여주기
+function printTime(){
+    hourSection.innerText = hour<10 ? '0'+hour : hour;
+    minSection.innerText = min<10 ? '0'+min : min;
+    secSection.innerText = sec<10 ? '0'+sec : sec;
+}
+
+printTime();
+
+//타이머 관련 함수
+function start(){
+    startTimer = setInterval(setTime, 1000);
+}
+
+function stop(){
+    clearInterval(startTimer);
+}
+
+function reset(){
+    sec = 0;
+    min = 0;
+    hour = 0;
+    printTime();
+    clearInterval(startTimer);
+}

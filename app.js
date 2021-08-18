ae4af754acbb32916eb35aa27d6cb1bccde446cd
Hwang yena <center7332@naver.com>
2021-08-18 20:09:28 +0900

    day07 - show id

--- app.js
+++ app.js
@@ -1,49 +1,9 @@
-let hourSection = document.getElementById("hour");
-let minSection = document.getElementById("min");
-let secSection = document.getElementById("sec");
+let idArray = ['y','e','s','m','e'];
+let position = 0;
 
-let hour = 0;
-let min = 0;
-let sec = 0;
-
-let startTimer;
-
-//타이머 증가
-function setTime(){
-    sec++;
-    if(sec>=60){
-        sec = 0;
-        min += 1;
-    }
-    if(min>=60){
-        min = 0;
-        hour += 1;
-    }
-    printTime();
-}
-
-//타이머 보여주기
-function printTime(){
-    hourSection.innerText = hour<10 ? '0'+hour : hour;
-    minSection.innerText = min<10 ? '0'+min : min;
-    secSection.innerText = sec<10 ? '0'+sec : sec;
-}
-
-printTime();
-
-//타이머 관련 함수
-function start(){
-    startTimer = setInterval(setTime, 1000);
-}
-
-function stop(){
-    clearInterval(startTimer);
-}
-
-function reset(){
-    sec = 0;
-    min = 0;
-    hour = 0;
-    printTime();
-    clearInterval(startTimer);
+function showId(){
+    if(position>=idArray.length) return;
+    
+    document.getElementById("id_section").innerText += idArray[position];
+    position++;
 }

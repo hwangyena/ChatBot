0f404a2c376203f30814c956e4b5df430f338359
hwangyena <center7332@naver.com>
2021-08-24 09:12:58 +0900

    day11 - chatbot 1

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
-function clickCssEvent(){
-    let click_btn = document.getElementById("phone_non_click");
-    click_btn.classList.add("phone_click");
-
-    window.setTimeout(()=>{
-        click_btn.classList.remove("phone_click");
-    }, 50);
+const quokaChat = {
+    0: "무슨 말인지 모르겠어",
+    1: "안녕!! 난 쿼카야",
+    2: "난 책 읽는걸 좋아해",
+    3: "밥 먹는 중!"
 }
 
-function btn(){
-    clickCssEvent();
-    window.setTimeout(()=>{
-        document.getElementById("number").style.visibility = "visible";
-    }, 1950);
+const userHi = [
+    "안녕",
+    "안녕!",
+    "반가워",
+    "안녕하세요"
+]
+
+const userHabbit = [
+    "취미가 뭐야?",
+    "뭐 좋아해?",
+    "좋아하는 거",
+    "취미"
+]
+
+const userDoing = [
+    "뭐하고있어",
+    "뭐하고 있어",
+    "뭐해?",
+    "뭐해",
+    "뭐하니"
+]
+
+function textShow(str){
+    let txt = document.getElementById("answer");
+    txt.innerText = str;
+}
+
+function chatting(){
+    let user = document.getElementById("user_text").value;
+
+    let answer = 0;
+    if(user.length === 0){
+        alert("message를 입력해주세요!");
+        return;
+    }
+    
+    if(userHi.includes(user)) answer = 1;
+    else if(userHabbit.includes(user)) answer = 2;
+    else if(userDoing.includes(user)) answer = 3;
+
+    console.log(quokaChat[answer]);
+    textShow(quokaChat[answer]);
 }
